18526723d32427189b498490bd4e7192afb6e2b7
eddylobaton <eddylobaton@gmail.com>
2025-04-22 22:04:28 -0500

    post ejecutar collectstatic railway v6

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,6 +129,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Para producción (cuando haces collectstatic):  # para producción   
+
+# Solo si estás usando archivos dentro de apps
+STATICFILES_DIRS = [
+    BASE_DIR / 'tienda' / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
